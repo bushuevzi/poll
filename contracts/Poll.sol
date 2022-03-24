dbab41bc84b71329f1af6640a0edd949949ffcc4
Zakhar Bushuev <z.bushuev@sdventures.com>
2022-03-25 02:27:13 +0300

    first version of contract is done

--- contracts/Poll.sol
+++ contracts/Poll.sol
@@ -20,14 +20,6 @@ contract Poll {
     string[] private pollsLUT;
     mapping (string => Poll) polls;
 
-    mapping (string => mapping (address => uint)) private pollsVotes;
-    mapping (string => mapping (address => bool)) private pollsMembers;
-    mapping (string => address[]) private pollsMembersLUT;
-    mapping (string => address[]) private pollsWiners;
-    mapping (string => uint) private pollsStartedTimes;
-    mapping (string => bool) private finnishedPolls;
-    mapping (string => uint) private pollsComissions;
-
     constructor() {
         owner = msg.sender;
     }
@@ -38,30 +30,29 @@ contract Poll {
         
         pollsLUT.push(pollName);
         polls[pollName].startTime = block.timestamp;
-        pollsStartedTimes[pollName] = block.timestamp;
     }
 
     function withdrawComission(string memory pollName) public {
         require(msg.sender == owner, "You should be contract owner");
-        require(pollsStartedTimes[pollName] > 0, "Poll not found");
-        require(finnishedPolls[pollName] == true, "Poll not finished");
-        require(pollsComissions[pollName] == 0, "Comission is empty");
+        require(polls[pollName].startTime > 0, "Poll not found");
+        require(polls[pollName].isFinished == true, "Poll not finished");
+        require(polls[pollName].comission == 0, "Comission is empty");
 
-        owner.call{value: pollsComissions[pollName]}("");
+        owner.call{value: polls[pollName].comission}("");
     }
 
-    function vote(string memory pollName, address likedCandidate) public payable {
+    function vote(string memory pollName, address favoriteCandidate) public payable {
         require(msg.value == voteCost, "You need to send 0.01 Ether");
 
         registerMemberInPoll(pollName, msg.sender);
-        pollsVotes[pollName][likedCandidate] += 1; // vote
+        polls[pollName].votes[favoriteCandidate] += 1; // vote
     }
 
     function finishPoll(string memory pollName) public {
-        require(pollsStartedTimes[pollName] > 0, "Poll not found");
-        require(finnishedPolls[pollName] == false, "Poll allrady finished");
-        require(pollsStartedTimes[pollName] + 3 days < block.timestamp, "Voting lasts less than 3 days");
-        require(pollsMembers[pollName][msg.sender] == true, "You not poll member");
+        require(polls[pollName].startTime  > 0, "Poll not found");
+        require(polls[pollName].isFinished == false, "Poll allrady finished");
+        require(polls[pollName].startTime  + 3 days < block.timestamp, "Voting lasts less than 3 days");
+        require(polls[pollName].members[msg.sender] == true, "You not poll member");
 
         uint _prize = getPrize(pollName);
         address payable _winner = payable(getWinner(pollName));
@@ -69,34 +60,38 @@ contract Poll {
         (bool result, bytes memory data) = _winner.call{value: _prize}("");
 
         if(result){
-            finnishedPolls[pollName] = true;
+            polls[pollName].isFinished = true;
         }
     }
 
     function getPolls() public view returns(string[] memory) {
-        return polls;
+        return pollsLUT;
     }
 
     function getPollMembers(string memory pollName) public view returns(address[] memory) {
-        return pollsMembersLUT[pollName];
+        return polls[pollName].membersLUT;
     }
 
-    function getPollVotes(string memory pollName) public view returns(address[] memory) {
-        return p[pollName];
+    function getPollWinner(string memory pollName) public view returns(address) {
+        return polls[pollName].winner;
+    }
+
+    function getMemberVotes(string memory pollName, address member) public view returns(uint) {
+        return polls[pollName].votes[member];
     }
 
     function registerMemberInPoll(string memory pollName, address member) private {
-        if(pollsMembers[pollName][member]) return;
+        if(polls[pollName].members[member]) return;
 
-        pollsMembers[pollName][member] = true;
-        pollsMembersLUT[pollName].push(member);
+        polls[pollName].members[member] = true;
+        polls[pollName].membersLUT.push(member);
     }
 
     function getPrize (string memory pollName) private returns (uint) {
-        uint _priseWithComission = pollsMembersLUT[pollName].length * voteCost;
+        uint _priseWithComission = polls[pollName].membersLUT.length * voteCost;
         uint _comission = calcComission(_priseWithComission);
         
-        pollsComissions[pollName] = _comission;
+        polls[pollName].comission = _comission;
 
         return _priseWithComission - _comission;
     }
@@ -107,12 +102,15 @@ contract Poll {
 
     function getWinner (string memory pollName) private returns (address) {
         address _winner;
-        uint maxVotes = 0;
+        uint _maxVotes = 0;
 
-        for(uint i = 0; i < pollsMembersLUT[pollName].length; i++){
-            address _member = pollsMembersLUT[pollName][i];
-            if(pollsVotes[pollName][_member] >= maxVotes) {
+        for(uint i = 0; i < polls[pollName].membersLUT.length; i++){
+            address _member = polls[pollName].membersLUT[i];
+            uint _memberVotes = polls[pollName].votes[_member];
+
+            if(_memberVotes >= _maxVotes) {
                 _winner = _member;
+                _maxVotes = _memberVotes;
             }
         }
 
